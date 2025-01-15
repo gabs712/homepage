b3234ce44437b9160dfd4b622ce0c2794231cf09
gabs <gabs712.g@gmail.com>
2025-01-15 20:30:37 -0300

    Implement header on small screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 const config = {
   darkMode: 'selector',
   content: ['./src/index.html', './src/js/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: ['Playfair Display', ...defaultTheme.fontFamily.serif],
+        roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
 }
 
